86b2df74ca4833569cfbbbb95966b44ca8a0e91d
Stephen S. Mitchell <stephen@stephensmitchell.com>
2024-05-12 05:18:29 -0500

    Initial setup for Visual Basic project
    Added and updated several files for the initial setup of a new Visual Basic project in Visual Studio. The `.gitignore` file has been updated to ignore unnecessary files. Added `ADDesignExplorerRefresh.sln` and `ADDesignExplorerRefresh.vbproj` files for project mapping. Also added `App.config` and `Init.vb` for application configuration and main entry point respectively. Auto-generated files by Visual Studio for various settings and resources have been added.
    
    In addition, a new `MySettings` class and `MySettingsProperty` module have been added to the `My` namespace for application settings management. An XML file `Settings.settings` has been added to define the settings for the application, currently no specific settings are defined.

---
+++ Init.vb
@@ -0,0 +1,32 @@
+﻿Imports AlibreX
+Imports System.Threading
+Module Init
+    Public Session As IADSession
+    Public DesignSession As AlibreX.IADDesignSession
+    Public FeatureStep As AlibreX.IADPartSession
+    Public Hook As IAutomationHook
+    Public Root As IADRoot
+    Sub Main()
+        Try
+            Hook = GetObject(, "AlibreX.AutomationHook")
+            Root = Hook.Root
+            Session = Root.TopmostSession
+            DesignSession = Session
+            FeatureStep = Session
+            FeatureStep.Features.Item(0).IsActive = True
+            Thread.Sleep(TimeSpan.FromSeconds(0.5))
+            For Each item As IADPartFeature In FeatureStep.Features
+                item.IsActive = True
+                Thread.Sleep(TimeSpan.FromSeconds(0.5))
+            Next
+            FeatureStep.RegenerateAll()
+            FeatureStep.Save()
+        Catch ex As ArgumentException
+            MsgBox(ex.Message)
+        Finally
+            Hook = Nothing
+            Root = Nothing
+        End Try
+    End Sub
+
+End Module
